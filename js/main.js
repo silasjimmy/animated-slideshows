c19de9f4f16372682dfc6e7d3fdc448eae4bc97c
Silas Jimmy <jimmysilas17@gmail.com>
2024-01-21 09:43:59 +0300

    refactor: remove fixed position on navbar

--- js/main.js
+++ js/main.js
@@ -2,25 +2,3 @@ $(document).ready(function () {
   $(".tabs").tabs();
   $(".collapsible").collapsible();
 });
-
-const navbarWrapper = document.querySelector(".navbar-fixed");
-const navbar = navbarWrapper.querySelector("nav");
-const largeLogo = navbarWrapper.querySelector(".logo");
-const smallLogo = navbarWrapper.querySelector(".brand-logo");
-
-window.addEventListener("scroll", () => {
-  if (
-    document.body.scrollTop > 100 ||
-    document.documentElement.scrollTop > 100
-  ) {
-    navbar.classList.remove("transparent");
-    navbarWrapper.classList.add("navbar--animate");
-    smallLogo.classList.remove("hide");
-    largeLogo.classList.add("hide");
-  } else {
-    navbarWrapper.classList.remove("navbar--animate");
-    navbar.classList.add("transparent");
-    smallLogo.classList.add("hide");
-    largeLogo.classList.remove("hide");
-  }
-});
